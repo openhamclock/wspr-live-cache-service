3b77e791ef2a3eb5637ed2b8155a5a66333ca7d0
Dave Koberstein <davek@komacke.com>
2026-05-29 07:13:24 -0700

    Pull under ohb build style
    Migrate to an ohb build style for image management, etc. Next step will
    be to roll docker compose into OHB's docker compose and to migrate this
    fetchWSPR.pl into OHB's.

---
+++ docker/build-image.sh
@@ -0,0 +1,169 @@
+#!/bin/bash
+# build voacap-server images for local and Docker Hub
+#
+# Copyright (C) 2026 Open HamClock Backend (OHB) Contributors
+# License: GNU Affero General Public License v3.0 (AGPLv3)
+# See LICENSE file or <https://www.gnu.org/licenses/agpl-3.0.html>
+#
+
+# Variables to set
+IMAGE_BASE=komacke/wspr-live-cache
+
+# Don't set anything past here
+TAG=$(git describe --exact-match --tags 2>/dev/null)
+if [ $? -ne 0 ]; then
+    echo "NOTE: Not currently on a tag. Using 'latest'."
+    TAG=latest
+    GIT_VERSION=$(git rev-parse --short HEAD)
+else
+    GIT_VERSION=$TAG
+fi
+
+IMAGE=$IMAGE_BASE:$TAG
+CONTAINER=${IMAGE_BASE##*/}
+
+# Get our directory locations in figured out
+HERE="$(realpath -s "$(dirname "$0")")"
+THIS="$(basename "$0")"
+cd $HERE
+
+usage() {
+    cat<<EOF
+$THIS: 
+
+    Builds the latest docker image based on the current git branch. It will figure out
+    if on a git tag and will use that for the docker image tag. Otherwise falls back
+    to 'latest'.
+
+    -m: multi-platform image buld for: linux/amd64 linux/arm64 linux/arm/v7
+        - argument is ignored when run with -c
+        - remember to setup a buildx container: 
+            docker buildx create --name ohb --driver docker-container --use
+            docker buildx inspect --bootstrap
+    -n: add --no-cache to build
+EOF
+    exit 0
+}
+
+main() {
+    RETVAL=0
+    MULTI_PLATFORM=false
+    NOCACHE=false
+
+    if [[ "$@" =~ --help ]]; then
+        usage
+    fi
+
+    while getopts ":p:cmh" opt; do
+        case $opt in
+            m)
+                MULTI_PLATFORM=true
+                ;;
+            n)
+                NOCACHE=true
+                ;;
+            h)
+                usage
+                ;;
+            \?) # Handle invalid options
+                echo "Invalid option: -$OPTARG" >&2
+                exit 1
+                ;;
+            :) # Handle options requiring an argument but none provided
+                echo "Option -$OPTARG requires an argument." >&2
+                exit 1
+                ;;
+        esac
+    done
+
+    do_all
+    build_done_message
+}
+
+do_all() {
+    warn_image_tag
+    warn_local_edits
+    build_image
+}
+
+warn_image_tag() {
+    if [ $TAG != latest ]; then
+        if [ $MULTI_PLATFORM == true ]; then
+            docker manifest inspect $IMAGE >/dev/null
+            if [ $? -eq 0 ]; then
+                echo
+                echo "WARNING: the multiplatform docker image for '$IMAGE' already exists in Docker Hub. Please"
+                echo "         remove it if you want to rebuild."
+                exit 2
+            fi
+        elif docker image list --format '{{.Repository}}:{{.Tag}}' | grep -qs $IMAGE; then
+            echo
+            echo "WARNING: the docker image for '$IMAGE' already exists. Please remove it if you want to rebuild."
+            exit 2
+        fi
+    fi
+}
+
+warn_local_edits() {
+    # check if there are local edits in the filesystem. We probably don't want to push them
+    git diff-index --quiet HEAD --
+    LOCAL_EDITS=$?
+
+    if [ $LOCAL_EDITS -ne 0 ]; then
+        if [ $MULTI_PLATFORM == true ]; then
+            echo
+            echo "ERROR: There are local edits. stash or reset them before pushing"
+            echo "       images to Docker Hub."
+            exit 3
+        else
+            echo
+            echo "WARNING: there are local edits. If you didn't intend that, stash"
+            echo "         them and build again."
+        fi
+    fi
+    return $LOCAL_EDITS
+}
+
+build_image() {
+    if [ $NOCACHE == true ]; then
+        NOCACHE_ARG="--no-cache"
+    fi
+    # Build the image
+    echo
+    echo "Building image for '$IMAGE_BASE:$TAG'"
+    pushd "$HERE/.." >/dev/null
+    if [ $MULTI_PLATFORM == true ]; then
+        docker buildx build \
+            $NOCACHE_ARG \
+            --pull \
+            --build-arg GIT_VERSION=${GIT_VERSION} \
+            -t $IMAGE \
+            -f docker/Dockerfile \
+            --platform linux/amd64,linux/arm64 \
+            --push \
+            .
+    else
+        docker build \
+            $NOCACHE_ARG \
+            --pull \
+            --build-arg GIT_VERSION=${GIT_VERSION} \
+            -t $IMAGE \
+            -f docker/Dockerfile \
+            .
+    fi
+    RETVAL=$?
+    popd >/dev/null
+}
+
+build_done_message() {
+    if [ $RETVAL -eq 0 ]; then
+        # basic info
+        echo
+        echo "Completed building '$IMAGE'."
+    else
+        echo "build failed with error: $RETVAL"
+    fi
+}
+
+main "$@"
+exit $RETVAL
